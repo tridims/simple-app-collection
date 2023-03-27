ec30afd5f9fb821b6b08b05b5e916acc911b348b
tridims <tri.dimas@student.ub.ac.id>
2023-03-27 09:38:02 +0700

    Adding Hapi Webserver to the project

---
+++ Hapi-Webserver/README.md
@@ -0,0 +1,3 @@
+# Simple Nodejs Webserver using Hapi Library.
+
+Made by followring Dicoding course about basic backend development.
---
+++ Hapi-Webserver/src/routes.js
@@ -0,0 +1,61 @@
+const routes = [
+
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if (lang === "id") {
+        return `Hai, ${name}!`;
+      }
+      return `Hello, ${name}!`;
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
+  {
+    method: "POST",
+    path: "/login",
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      return `Welcome ${username}!`;
+    },
+  },
+];
+
+module.exports = routes;
